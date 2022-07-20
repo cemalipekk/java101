ea4f12c4eb37b72ad37e7d8a68a55f79147d4116
cemalipekk <cipek.4569@gmail.com>
2022-07-20 23:12:37 +0300

    www.patika.dev Tek Sayıların Toplamını Bulan Program
    Tek Sayıların Toplamını Bulan Program Ödevi

---
+++ TekSayiBulanProgram.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class TekSayiBulanProgram {
+    public static void main(String[] args) {
+        int k,toplam = 0;
+
+        Scanner inp = new Scanner(System.in);
+        do {
+            System.out.print("Sayi giriniz : ");
+            k = inp.nextInt();
+            if (k % 4== 0){
+                toplam+= k;
+            }
+
+
+
+
+        }while(k % 2 ==0);
+        System.out.println("Toplam : " + toplam);
+
+
+    }
+}
