99ff7688ba57deee595f0c853185b864f07210e4
cemalipekk <cipek.4569@gmail.com>
2022-10-22 15:54:39 +0300

    www.patika.dev MayinTarlasiOyunu
    MayinTarlasiOyunu

---
+++ MayinTarlasiOyunu/src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+     public static void main(String[] args) {
+        MineSwepper mineSwepper = new MineSwepper(4,4);
+        mineSwepper.run();
+    }
+}
